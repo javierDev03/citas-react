47a55e3ba9f4aa177165467b76bb51c56d30bf5b
javier cervantes <javicervel250@gmail.com>
2024-10-12 21:09:30 -0600

    first commit

--- veterinaria-backend/src/main/java/com/veterinaria/veterinaria_backend/repository/PacienteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.veterinaria.veterinaria_backend.repository;
-
-import com.veterinaria.veterinaria_backend.models.Paciente;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PacienteRepository extends MongoRepository<Paciente, String> {}    
